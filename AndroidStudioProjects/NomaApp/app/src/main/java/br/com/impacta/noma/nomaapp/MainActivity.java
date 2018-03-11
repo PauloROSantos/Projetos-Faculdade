ee9462d094648f5c9a1786491aec01361cbcaff0
PauloROSantos <pp_prx@hotmail.com>
2018-03-10 21:59:59 -0300

    Adicionando trabalho AndroidStudio @ Noma

---
+++ AndroidStudioProjects/NomaApp/app/src/main/java/br/com/impacta/noma/nomaapp/MainActivity.java
@@ -0,0 +1,37 @@
+package br.com.impacta.noma.nomaapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+    }
+    public void telaLogin(View view){
+        setContentView(R.layout.login);
+    }
+
+    public void logar(View view){
+        Intent it = new Intent(MainActivity.this,logado.class);
+
+        EditText loginDado = (EditText) findViewById(R.id.login_dado);
+        String login = loginDado.getText().toString();
+
+        EditText senhaDado = (EditText) findViewById(R.id.senha_dado);
+        String senha = senhaDado.getText().toString();
+
+        it.putExtra("login",login );
+        it.putExtra("senha",senha );
+        startActivity(it);
+    }
+
+}
---
+++ AndroidStudioProjects/NomaApp/app/src/main/java/br/com/impacta/noma/nomaapp/logado.java
@@ -0,0 +1,27 @@
+package br.com.impacta.noma.nomaapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class logado extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_logado);
+
+        Bundle bundle = getIntent().getExtras();
+        if(bundle != null){
+            String login = bundle.getString("login");
+            String senha = bundle.getString("senha");
+
+            TextView bemVindo = (TextView) findViewById(R.id.bemVindo);
+            bemVindo.setText("Bem vindo "+login+" sua senha é "+senha);
+        }
+
+    }
+}
